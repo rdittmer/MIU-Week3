6398d4c55dabf7b22b8242b91eece65cfe43c534
Ryan Dittmer <rdittmer@fullsail.edu>
2012-06-15 22:16:37 -0400

    making sure repository is up to date

---
+++ Gold/js/main.js
@@ -0,0 +1,9 @@
+$(document).bind('pageinit', function(){
+   var form = $('teeForm');
+   form.validate({
+      invalidHandler: function(form, validator){},
+      submitHandler: function(){
+         
+      }
+   });
+});
